99fe00bcaf5a8e9e7887de4f8ea620a6581a12f2
Miguel Ramos <return.infox@gmail.com>
2026-01-06 20:24:17 -0300

    objetos

--- js/2-objetos.js
+++ js/2-objetos.js
@@ -1,12 +1,17 @@
 const curso = {
   titulo: 'JavaScript moderno',
   duracion: '20 horas',
-  instructor: 'Catalina'
+  instructor: 'Catalina',
+  metadatos: {
+    nivel: 'principiante',
+    idioma: 'Español'
+  }
 };
 
 console.log(curso.titulo);
 console.log(`Estamos en el curso: ${curso.titulo}`);
 console.log(`Lo dicta la instructora: ${curso.instructor}`);
 console.log(`Tiene una duración de: ${curso.duracion}`);
+console.log(`El curso se dictara en el idioma: ${curso.metadatos.idioma}`);
 // clave: valor
 // nombre: 'sofia'
