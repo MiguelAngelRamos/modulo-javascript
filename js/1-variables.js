0cb3e45d5cbef11ccf60b5b0231980751e196ab6
Miguel Ramos <return.infox@gmail.com>
2026-01-06 20:20:01 -0300

    proyecto base para actividad

---
+++ js/1-variables.js
@@ -0,0 +1,23 @@
+// let o const
+// pero nunca var
+let name = "Sofia";
+name = "Marcela";
+name = 20;
+// console.log(name);
+
+const PI = 3.14;
+const DNI = 12939193;
+const PATENTE = 'AG-23103';
+const NUMEROCASA ='21313-A';
+// console.log(PI)
+
+// Dinamico no es de tipado Estricto
+
+let usuario = "Vicky";
+let role = "ADMIN";
+
+console.log('El usuario: '+ usuario + ', tiene el rol: ' + role); // concatenación
+
+console.log(`El usuario: ${usuario}, tiene el rol: ${role}`);
+
+console.log('El usuario: ${usuario}, tiene el rol: ${role}')
---
+++ js/2-objetos.js
@@ -0,0 +1,12 @@
+const curso = {
+  titulo: 'JavaScript moderno',
+  duracion: '20 horas',
+  instructor: 'Catalina'
+};
+
+console.log(curso.titulo);
+console.log(`Estamos en el curso: ${curso.titulo}`);
+console.log(`Lo dicta la instructora: ${curso.instructor}`);
+console.log(`Tiene una duración de: ${curso.duracion}`);
+// clave: valor
+// nombre: 'sofia'
