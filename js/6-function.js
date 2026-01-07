4d051e0848b87b93494e36181d74f16fb720db19
Miguel Ramos <return.infox@gmail.com>
2026-01-07 20:03:17 -0300

    funcion sumar

---
+++ js/6-function.js
@@ -0,0 +1,9 @@
+function sumar(number1, number2) {
+  // Proceso
+  result = number1 + number2;
+  return console.log(`El resultado de la suma entre ${number1} y ${number2} es: ${result}`);
+}
+
+console.log(sumar(10, 5));
+console.log(sumar(50, 30));
+console.log(sumar(7, 5));
